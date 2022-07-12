3cbe7773d4e99bb3e266512be198608033f54c15
lenguyenphuckhang2001 <80748946+lenguyenphuckhang2001@users.noreply.github.com>
2022-07-12 18:28:33 +0700

    Add files via upload

---
+++ Menu/Menu/MainSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    class MainSystem
+    {
+        Menu myMenu = new DefaultMenu();
+        Menu myChickenRice = new ChickenRice();
+        Menu myMeatRice = new MeatRice();
+        Menu myEggRice = new EggRice();
+        Menu myMixedRice = new MixedRice();
+
+        static void Main(string[] args)
+        {
+            MainSystem mainSystem = new MainSystem();
+            mainSystem.DisplayMenu();
+        }
+        public void DisplayMenu()
+        {
+            Menu myWaterSpinach = new WaterSpinach(myMenu);
+            Menu myOmelet = new Omelet(myMenu);
+            Menu mySoup = new Soup(myMenu);
+            Menu mySoftDrink = new SoftDrink(myMenu);
+
+            Console.WriteLine("*=*=*=*=*=*= Menu Traditional Food =*=*=*=*=*=*\n");
+            //Main course
+            Console.WriteLine("=================== Main Course ===================");
+            Console.WriteLine($"{myChickenRice.getDescription()}: ${myChickenRice.price()}");
+            Console.WriteLine($"{myMeatRice.getDescription()}: ${myMeatRice.price()}");
+            Console.WriteLine($"{myEggRice.getDescription()}: ${myEggRice.price()}");
+            Console.WriteLine($"{myMixedRice.getDescription()}: ${myMixedRice.price()}\n");
+            //Side Dish
+            Console.WriteLine("=================== Side Dish ===================");
+            Console.WriteLine($"{myWaterSpinach.getDescription()}: ${myWaterSpinach.price()}");
+            Console.WriteLine($"{myOmelet.getDescription()}: ${myOmelet.price()}");
+            Console.WriteLine($"{mySoup.getDescription()}: ${mySoup.price()}");
+            Console.WriteLine($"{mySoftDrink.getDescription()}: ${mySoftDrink.price()}");
+        }
+    }
+}
+
---
+++ Menu/Menu/Menu.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    public abstract class Menu
+    {
+        protected string description = "Default";
+        public virtual string getDescription()
+        {
+            return description;
+        }
+        public abstract double price();
+    }
+
+    public class DefaultMenu : Menu
+    {
+        public DefaultMenu()
+        {
+            description = "";
+        }
+        public override double price()
+        {
+            return 0;
+        }
+    }
+
+    //Order Menu
+    public class ChickenRice : Menu
+    {
+        public ChickenRice()
+        {
+            description = "Chicken Rice";
+        }
+        public override double price()
+        {
+            return 35.000;
+        }
+    }
+
+    public class MeatRice : Menu
+    {
+        public MeatRice()
+        {
+            description = "Meat Rice";
+        }
+        public override double price()
+        {
+            return 25.000;
+        }
+    }
+
+    public class EggRice : Menu
+    {
+        public EggRice()
+        {
+            description = "Egg Rice";
+        }
+        public override double price()
+        {
+            return 20.000;
+        }
+    }
+
+    public class MixedRice : Menu
+    {
+        public MixedRice()
+        {
+            description = "Egg Rice";
+        }
+        public override double price()
+        {
+            return 30.000;
+        }
+    }
+
+    //Order more side dishes
+    public class WaterSpinach : Menu
+    {
+        Menu myMenu;
+        public WaterSpinach(Menu menu)
+        {
+            this.myMenu = menu;
+        }
+        public override string getDescription()
+        {
+            return myMenu.getDescription() + "Water Spinach";
+        }
+        public override double price()
+        {
+            return myMenu.price() + 3.000;
+        }
+    }
+
+    public class Omelet : Menu
+    {
+        Menu myMenu;
+        public Omelet(Menu menu)
+        {
+            this.myMenu = menu;
+        }
+        public override string getDescription()
+        {
+            return myMenu.getDescription() + "Omelet";
+        }
+        public override double price()
+        {
+            return myMenu.price() + 6.000;
+        }
+    }
+
+    public class Soup : Menu
+    {
+        Menu myMenu;
+        public Soup(Menu menu)
+        {
+            this.myMenu = menu;
+        }
+        public override string getDescription()
+        {
+            return myMenu.getDescription() + "Soup";
+        }
+        public override double price()
+        {
+            return myMenu.price() + 5.000;
+        }
+    }
+
+    public class SoftDrink : Menu
+    {
+        Menu myMenu;
+        public SoftDrink(Menu menu)
+        {
+            this.myMenu = menu;
+        }
+        public override string getDescription()
+        {
+            return myMenu.getDescription() + "SoftDrink";
+        }
+        public override double price()
+        {
+            return myMenu.price() + 10.000;
+        }
+    }
+
+
+}
